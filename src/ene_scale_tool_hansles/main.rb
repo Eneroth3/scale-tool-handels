eaf0fad34bf8ff59e0145ccbb2a07cf423d002f5
Eneroth3 <eneroth3@gmail.com>
2017-10-04 13:31:30 +0200

    Add plugin reload method

--- src/ene_scale_tool_hansles/main.rb
+++ src/ene_scale_tool_hansles/main.rb
@@ -116,6 +116,29 @@ module EneScaleToolHandles
 
   end
 
+  # Reload whole extension (except loader) without littering
+  # console. Inspired by ThomTohm's method.
+  # Only works before extension has been scrambled.
+  #
+  # clear_console - Clear console from previous content too (default: false)
+  #
+  # Returns nothing.
+  def self.reload(clear_console = false, undo = false)
+    # Hide warnings for already defined constants.
+    verbose = $VERBOSE
+    $VERBOSE = nil
+    Dir.glob(File.join(PLUGIN_DIR, "*.rb")).each { |f| load(f) }
+    $VERBOSE = verbose
+
+    # Use a timer to make call to method itself register to console.
+    # Otherwise the user cannot use up arrow to repeat command.
+    UI.start_timer(0) { SKETCHUP_CONSOLE.clear } if clear_console
+
+    Sketchup.undo if undo
+
+    nil
+  end
+
   UI.add_context_menu_handler do |menu|
     model = Sketchup.active_model
     next unless model.selection.size == 1
